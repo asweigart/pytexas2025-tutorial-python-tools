14e5178f04f494088a2d0fe4f3c14ac6bbeacd63
Al Sweigart <asweigart@gmail.com>
2025-04-09 11:42:59 -0400

    updates

--- moon_app/moon_app.py
+++ moon_app/moon_app.py
@@ -1,6 +1,6 @@
-import exampleskypackage
+import examplesky
 
-if exampleskypackage.__version__ != '1.0':
-    raise Exception('The moon app requires version 1.0 of exampleskypackage!')
+if examplesky.__version__ != '1.0':
+    raise Exception('The moon app requires version 1.0 of examplesky!')
 
 print('Success.')
